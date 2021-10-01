cf7eecfa9773e627ee54ff2b74434accfbe660e1
Ales <codegod313@gmail.com>
2021-10-01 10:39:09 +0300

    Added test to ExtremumService

--- src/test/java/edu/epam/firsttask/service/impl/common/ExtremumServiceImplTest.java
+++ src/test/java/edu/epam/firsttask/service/impl/common/ExtremumServiceImplTest.java
@@ -1,14 +1,53 @@
 package edu.epam.firsttask.service.impl.common;
 
+import edu.epam.firsttask.entity.CustomArray;
+import edu.epam.firsttask.exception.EmptyArrayException;
+import edu.epam.firsttask.service.ExtremumService;
 import junit.framework.TestCase;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class ExtremumServiceImplTest extends TestCase {
 
+    static Logger logger = LogManager.getLogger(ExtremumServiceImplTest.class);
 
+    ExtremumService extremumService;
 
-    public void testGetMin() {
+    @Before
+    public void setUp(){
+        extremumService = new ExtremumServiceImpl();
     }
 
+    @Test
+    public void testGetMin() {
+        Double[] values = {555.5, 777.7};
+        CustomArray customArray = new CustomArray(Arrays.asList(values));
+        Double expected = 555.5;
+        Double actual = null;
+        try {
+            actual = extremumService.getMin(customArray);
+        } catch (EmptyArrayException e) {
+            logger.error("Array is empty", e);
+        }
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void testGetMax() {
+        Double[] values = {555.5, 777.7};
+        CustomArray customArray = new CustomArray(Arrays.asList(values));
+        Double expected = 777.7;
+        Double actual = null;
+        try {
+            actual = extremumService.getMax(customArray);
+        } catch (EmptyArrayException e) {
+            logger.error("Array is empty", e);
+        }
+        Assert.assertEquals(expected, actual);
     }
 }
